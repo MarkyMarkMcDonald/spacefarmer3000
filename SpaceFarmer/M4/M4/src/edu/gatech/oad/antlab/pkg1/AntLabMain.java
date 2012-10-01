e16aa381bc1ce1f029d0dc420151cb58e1bb4985
andrew.m.wilder <andrew.m.wilder@gmail.com>
2012-10-01 19:01:02 -0400

    Trying a third time to commit changes to my buildfile

--- SpaceFarmer/M4/M4/src/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ SpaceFarmer/M4/M4/src/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -60,7 +60,7 @@ import edu.gatech.oad.antlab.pkg2.*;
 		  //Person1 replace P1 with your name
 		  //and ggg000x with your gt num
 		  Person1 p1 = new Person1("Andrew Wilder");
-		  toPrint += p1.toString("awilder6");
+		  toPrint += p1.toString("902588570");
 		  //Person2 replace P2 with your name
 		  //and ggg000x with your gt num
 		  Person2 p2 = new Person2("Bobbey Reese");
