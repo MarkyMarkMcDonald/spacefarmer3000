aa1eb36b9dcf1b23131b2651fc43c80662e5133a
mark.mcdonald.ga <mark.mcdonald.ga@gmail.com>
2012-10-18 16:29:02 -0400

    made enumerations backed by strings (mainly for logging)

--- SpaceFarmer/src/App/model/ShipModel.java
+++ SpaceFarmer/src/App/model/ShipModel.java
@@ -2,8 +2,19 @@ package App.model;
 
 public enum ShipModel
 {
-    GNAT,
-    FIREFLY,
-    MOSQUITO,
-	BUMBLEBEE
+    GNAT("Gnat"),
+    FIREFLY("Firefly"),
+    MOSQUITO("Mosquito"),
+	BUMBLEBEE("Bumblebee");
+
+
+    private final String name;
+
+    private ShipModel(String name) {
+        this.name = name;
+    }
+
+    public String toString(){
+        return name;
+    }
 }
--- SpaceFarmer/src/App/model/SkillType.java
+++ SpaceFarmer/src/App/model/SkillType.java
@@ -1,13 +1,25 @@
 package App.model;
 
+import App.view.Screen;
+
 /**
  * User: marky
  * Date: 10/7/12
  * Time: 8:46 PM
  */
 public enum SkillType {
-    PILOTING,
-    TRADING,
-    FIGHTING,
-    ENGINEERING
+    PILOTING("Piloting"),
+    TRADING("Trading"),
+    FIGHTING("Fighting"),
+    ENGINEERING("Engineering");
+
+    private final String name;
+
+    private SkillType(String name) {
+        this.name = name;
+    }
+
+    public String toString(){
+        return name;
+    }
 }
