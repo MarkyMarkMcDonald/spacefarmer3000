24aa9470bdc092fa5700d83bd9509a279156e23a
andrew.m.wilder <andrew.m.wilder@gmail.com>
2012-11-04 20:47:06 -0500

    Updated transportation service

--- SpaceFarmer/src/App/service/TransportationService.java
+++ SpaceFarmer/src/App/service/TransportationService.java
@@ -1,7 +1,10 @@
 package App.service;
 
+import java.awt.geom.Point2D;
+
 import App.model.Game;
 import App.model.Planet;
+import App.model.PlanetarySystem;
 import App.model.Player;
 import App.view.Display;
 
@@ -16,25 +19,36 @@ public class TransportationService {
 	 * @param p The Planet to travel to.
 	 */
 	public static void goToPlanet(Planet p) {
-		// TODO check conditions that allow travel between planets
-		
-		/* Need to check the current player's fuel level
-		 * Compare the player's fuel level against what is needed to reach Planet p
-		 */
+
+		// Get data required to make the decision to travel
 		Player currentPlayer = Game.getCurrentPlayer();
+		PlanetarySystem newSystem = p.getPlanetarySystem();
+		PlanetarySystem currSystem = currentPlayer.getCurrentPlanet().getPlanetarySystem();
+		double distance = Point2D.distance(currSystem.getX(), currSystem.getY(), newSystem.getX(), newSystem.getY());
 		
-		boolean condition = true;
-		if(condition) {
-			// Travel successful
+		// If the planet is in the same system
+		if(newSystem != currSystem) {
 			
-			// If p is not in the Player's current Planetary System, then activate minigame
-			if(p.getPlanetarySystem().equals(currentPlayer.getCurrentPlanet().getPlanetarySystem())) {
+			// Travel to the planet
+			currentPlayer.setCurrentPlanet(p);
+		} else { // If the planet is in a different system
+			
+			// Check for enough fuel to make the journey
+			if(currentPlayer.getFuel() >= distance) {
+				
+				// Travel to the planet
+				currentPlayer.setCurrentPlanet(p);
+				
+				// Decrease fuel level
+				currentPlayer.setFuel(currentPlayer.getFuel() - (int)distance);
+				
+				// Play asteroid dodge minigame
 				Display.playMiniGame(); // Results from the minigame are handled in Display.exitGame()
+			} else {
+				// Travel unsuccessful
+				
+				// TODO: Give a message to the user that you couldn't travel
 			}
-		} else {
-			// Travel unsuccessful
-			
-			// TODO: Give a message to the user that you couldn't travel
 		}
 	}
 }
