d2e1be55f9498d1d5a57549313fad19ec711b710
mthomas46 <afroking42@gmail.com>
2012-11-08 17:24:37 -0500

    Loader added

--- SpaceFarmer/src/App/service/Loader_db.java
+++ SpaceFarmer/src/App/service/Loader_db.java
@@ -1,5 +1,158 @@
 package App.service;
 
+import java.io.File;
+
+import org.tmatesoft.sqljet.core.SqlJetException;
+import org.tmatesoft.sqljet.core.table.ISqlJetCursor;
+import org.tmatesoft.sqljet.core.table.ISqlJetTable;
+import org.tmatesoft.sqljet.core.table.SqlJetDb;
+
 public class Loader_db {
+	
+	String saveName;
+	String saveLocation;
+	/*
+	Collection <Player> players;
+	Collection <Planet> planets;
+	Settings gameSettings;
+	*/
+	//name of the database
+	private static String DB_NAME = "db.sqlite";
+	//table names
+    private static final String TABLE_PLAYERS = "Players";
+    private static final String TABLE_INVENTORY = "Inventory";
+    private static final String TABLE_PLANETS = "Planets";
+    private static final String TABLE_SETTINGS = "Game";
+    //player colls
+    //private static final String FIELD_INDEX = "NUMBER";
+    private static final String FIELD_NAME = "Name";
+    private static final String FIELD_PILOTING = "Piloting";
+    private static final String FIELD_TRADING = "Trading";
+    private static final String FIELD_ENGINEERING = "Engineering";
+    private static final String FIELD_FIGHTING = "Fighting";
+    private static final String FIELD_MONEY = "Money";
+    private static final String FIELD_SHIP = "Ship";
+    private static final String FIELD_CURRPLANET = "Current_Planet";
+
+    //Inventory colls
+    /*
+    private static final String FIELD_ = "";
+    private static final String FIELD_ = "";
+    private static final String FIELD_ = "";
+    private static final String FIELD_ = "";
+    private static final String FIELD_ = "";
+    private static final String FIELD_ = "";
+    private static final String FIELD_ = "";
+	*/
+    //Planet colls
+    private static final String FIELD_PLANET = "name";
+    private static final String FIELD_TECH = "Tech_LV";
+    private static final String FIELD_POLSYS = "Political_sys";
+    private static final String FIELD_RESOURCE = "Resource_Type";
+    private static final String FIELD_X = "X";
+    private static final String FIELD_Y = "Y";
+    //Game
+    private static final String FIELD_CURRTURN = "Current_Turn";
+    private static final String FIELD_CURRPLAYER = "Current_player";
+    private static final String FIELD_DIFF = "Difficulty";
+    private static final String FIELD_XDIM = "X_dimension";
+    private static final String FIELD_YDIM = "y_diminsion";
+	
+	public void LoadGame()throws SqlJetException{
+		File dbFile = new File(DB_NAME);
+	    SqlJetDb db = SqlJetDb.open(dbFile, true);
+        //db.beginTransaction(SqlJetTransactionMode.READ_ONLY);
+
+		//LoadPlayers(db.getTable(TABLE_PLAYERS));
+		
+		LoadPlanets(db.getTable(TABLE_PLANETS));
+		/*
+        db.beginTransaction(SqlJetTransactionMode.READ_ONLY);
+
+		LoadInventory(db.getTable(TABLE_INVENTORY));
+        db.beginTransaction(SqlJetTransactionMode.READ_ONLY);
+
+		LoadPlanets(db.getTable(TABLE_PLANETS));
+        db.beginTransaction(SqlJetTransactionMode.READ_ONLY);
+
+		LoadSettings(db.getTable(TABLE_SETTINGS));
+		 */
+	}
+	private void LoadPlayers(ISqlJetTable tbl )throws SqlJetException{
+		ISqlJetCursor cursor=tbl.open();
+		 try {
+	            if (!cursor.eof()) {
+	                do {
+	                	System.out.println("this is a test");
+	                    System.out.println(cursor.getRowId()	+ " " + 
+	                            cursor.getString(FIELD_NAME)	+ " " + 
+	                            cursor.getString(FIELD_MONEY)	+ " " + 
+	                            cursor.getString(FIELD_SHIP)	+ " " + 
+	                            cursor.getString(FIELD_CURRPLANET)	+ " "+
+	                            cursor.getString(FIELD_PILOTING)	+ " " + 
+	                            cursor.getString(FIELD_FIGHTING)	+ " " + 
+	                            cursor.getString(FIELD_ENGINEERING)	+ " " + 
+	                            cursor.getString(FIELD_TRADING)	+ " "
+	                            );
+	                } while(cursor.next());
+	            }
+	        } 
+		 finally {cursor.close();}
+	}
+	private void LoadInventory(ISqlJetTable tbl)throws SqlJetException{
+		ISqlJetCursor cursor=tbl.open();
+		 try {
+	            if (!cursor.eof()) {
+	                do {/*
+	                	System.out.println("this is a test");
+	                    System.out.println(cursor.getRowId()	+ " " + 
+	                            cursor.getString()	+ " " + 
+	                            cursor.getString()	+ " " + 
+	                            cursor.getString()	+ " " + 
+	                            cursor.getString()	+ " "
+	                            );*/
+	                } while(cursor.next());
+	            }
+	        } 
+		 finally {cursor.close();}
+	}
+	private void LoadPlanets(ISqlJetTable tbl)throws SqlJetException{
+		ISqlJetCursor cursor=tbl.open();
+		 try {
+	            if (!cursor.eof()) {
+	                do {
+	                	System.out.println("this is a test");
+	                    System.out.println(cursor.getRowId()	+ " " + 
+	                            cursor.getString(FIELD_PLANET)	+ " " + 
+	                            cursor.getString(FIELD_TECH)	+ " " + 
+	                            cursor.getString(FIELD_POLSYS)	+ " " + 
+	                            cursor.getString(FIELD_RESOURCE)	+ " "+
+	                            cursor.getString(FIELD_X)	+ " " + 
+	                            cursor.getString(FIELD_Y)	+ " " 
+	                            );
+	                } while(cursor.next());
+	            }
+	        } 
+		 finally {cursor.close();}
+	}
+	private void LoadSettings(ISqlJetTable tbl)throws SqlJetException{
+		ISqlJetCursor cursor=tbl.open();
+		 try {
+	            if (!cursor.eof()) {
+	                do {	               
+	                	System.out.println("this is a test");
+	                    System.out.println(cursor.getRowId()	+ " " + 
+	                            cursor.getString(FIELD_CURRTURN)	+ " " + 
+	                            cursor.getString(FIELD_CURRPLAYER)	+ " " + 
+	                            cursor.getString(FIELD_DIFF)	+ " "
+	                            );
+	                } while(cursor.next());
+	            }
+	        } 
+		 finally {cursor.close();}
+	}
+	
+	
+	
 
 }
