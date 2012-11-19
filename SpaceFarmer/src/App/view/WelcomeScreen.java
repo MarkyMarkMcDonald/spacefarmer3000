11e2132a049eb9cf1a7319e9c51a8bdf9cc0c272
mthomas46 <afroking42@gmail.com>
2012-11-18 21:06:01 -0500

    welcome screed audited by mykal
    Signed-off-by: mthomas46 <afroking42@gmail.com>

--- SpaceFarmer/src/App/view/WelcomeScreen.java
+++ SpaceFarmer/src/App/view/WelcomeScreen.java
@@ -18,6 +18,7 @@ import javax.swing.JPanel;
  * This class represents the panel shown to users when loading SpaceFarmer 3000.
  * 
  * @author Mark McDaniel, Andrew Wilder
+ * @version 1
  */
 public class WelcomeScreen extends Screen {
 
@@ -26,68 +27,78 @@ public class WelcomeScreen extends Screen {
 	 */
 	private static final long serialVersionUID = -4701770682971357648L;
 
+	/**
+	 * This serves as the welcome screen for the program
+	 */
 	public WelcomeScreen() {
 		name = CardName.WELCOME_CARD;
 
 		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 
-		Component titleStrut1 = Box.createVerticalStrut(40);
+		final Component titleStrut1 = Box.createVerticalStrut(40);
 		add(titleStrut1);
 
-		JLabel welcomeLabel1 = new JLabel("Space");
+		final JLabel welcomeLabel1 = new JLabel("Space");
 		welcomeLabel1.setAlignmentX(Component.CENTER_ALIGNMENT);
 		welcomeLabel1.setAlignmentY(0.0f);
 		welcomeLabel1.setFont(new Font("Tahoma", Font.BOLD, 40));
 		add(welcomeLabel1);
 
-		Component titleStrut2 = Box.createVerticalStrut(40);
+		final Component titleStrut2 = Box.createVerticalStrut(40);
 		add(titleStrut2);
 
-		JLabel welcomeLabel2 = new JLabel("Farmer");
+		final JLabel welcomeLabel2 = new JLabel("Farmer");
 		welcomeLabel2.setAlignmentX(Component.CENTER_ALIGNMENT);
 		welcomeLabel2.setAlignmentY(0.0f);
 		welcomeLabel2.setFont(new Font("Tahoma", Font.BOLD, 40));
 		add(welcomeLabel2);
 
-		Component titleStrut3 = Box.createVerticalStrut(40);
+		final Component titleStrut3 = Box.createVerticalStrut(40);
 		add(titleStrut3);
 
-		JLabel welcomeLabel3 = new JLabel("3000");
+		final JLabel welcomeLabel3 = new JLabel("3000");
 		welcomeLabel3.setAlignmentX(Component.CENTER_ALIGNMENT);
 		welcomeLabel3.setAlignmentY(0.0f);
 		welcomeLabel3.setFont(new Font("Tahoma", Font.BOLD, 40));
 		add(welcomeLabel3);
 
-		Component titleStrut4 = Box.createVerticalStrut(40);
+		final Component titleStrut4 = Box.createVerticalStrut(40);
 		add(titleStrut4);
 
-		JPanel welcomeButtonPanel = new JPanel();
+		final JPanel welcomeButtonPanel = new JPanel();
 		add(welcomeButtonPanel);
 
-		JButton AboutButton = new JButton("About");
-		AboutButton.setPreferredSize(new Dimension(120, 30));
-		welcomeButtonPanel.add(AboutButton);
+		final JButton aboutButton = new JButton("About");
+		aboutButton.setPreferredSize(new Dimension(120, 30));
+		welcomeButtonPanel.add(aboutButton);
 
-		Component buttonStrut1 = Box.createHorizontalStrut(50);
+		final Component buttonStrut1 = Box.createHorizontalStrut(50);
 		welcomeButtonPanel.add(buttonStrut1);
 
-		JButton newGameButton = new JButton("New Game");
+		final JButton newGameButton = new JButton("New Game");
 		newGameButton.setPreferredSize(new Dimension(120, 30));
 		newGameButton.addActionListener(new BasicContinueListener(
 				CardName.PLAYER_INFORMATION_CARD));
 		welcomeButtonPanel.add(newGameButton);
 
-		Component buttonStrut2 = Box.createHorizontalStrut(50);
+		final Component buttonStrut2 = Box.createHorizontalStrut(50);
 		welcomeButtonPanel.add(buttonStrut2);
 
-		JButton loadGameButton = new JButton("Load Game");
+		final JButton loadGameButton = new JButton("Load Game");
 		loadGameButton.setPreferredSize(new Dimension(120, 30));
 		loadGameButton.addActionListener(new TEMP_LISTENER());
 		welcomeButtonPanel.add(loadGameButton);
 	}
 
-	private class TEMP_LISTENER implements ActionListener {
+	/**
+	 * this listens for the buttons on the main screen
+	 * @author Mark
+	 */
+	private static class TEMP_LISTENER implements ActionListener {
 		@Override
+		/**
+		 * when a button is pressed
+		 */
 		public void actionPerformed(ActionEvent e) {
 			Display.playMiniGame();
 		}
@@ -95,7 +106,7 @@ public class WelcomeScreen extends Screen {
 		/**
 		 * @return Information about this object as a String.
 		 */
-		public String toString() {
+		@Override public String toString() {
 			return "TempListener";
 		}
 	}
