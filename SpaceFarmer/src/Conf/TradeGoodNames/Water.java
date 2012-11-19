6f70e9a61fe13eb29749af94be44edd82b8f348e
mthomas46 <afroking42@gmail.com>
2012-11-18 21:08:59 -0500

    water audited by me
    Signed-off-by: mthomas46 <afroking42@gmail.com>

--- SpaceFarmer/src/Conf/TradeGoodNames/Water.java
+++ SpaceFarmer/src/Conf/TradeGoodNames/Water.java
@@ -8,16 +8,32 @@ public enum Water {
 	SPRING_WATER("Spring Water"), UNFILTERED_WATER("Flavored Water"), SPORTS_DRINKS(
 			"Sports Drinks");
 
-	private String name;
+	/**
+	 * name of the water
+	 */
+	private final String name;
 
+	/**
+	 * returns the name
+	 * @return 
+	 * 	the name of the water good
+	 */
 	public String getName() {
 		return name;
 	}
 
+	/**
+	 * setter for the water object
+	 * @param name
+	 * 	name of the object
+	 */
 	private Water(String name) {
 		this.name = name;
 	}
 
+	/**
+	 * override for the to string method
+	 */
 	public String toString() {
 		return name;
 	}
