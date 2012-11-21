1e70ddfecc5893be7e6ac695f10188e88665e769
partiallyderived <partiallyderived@gmail.com>
2012-11-21 03:42:14 -0500

    Fixed most violations in app.view.market

--- SpaceFarmer/src/app/view/market/HeadingPanel.java
+++ SpaceFarmer/src/app/view/market/HeadingPanel.java
@@ -1,45 +1,104 @@
+/*This files holds the class HeadingPanel, which heads
+ * the market views.
+ */
 package app.view.market;
 
-import javax.swing.*;
-import java.awt.*;
+import java.awt.Component;
+import java.awt.Dimension;
+
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 /**
  * Created with IntelliJ IDEA. User: mark.mcdonald Date: 11/6/12 Time: 12:14 AM
  * To change this template use File | Settings | File Templates.
+ * 
+ * @author Mark
+ * @version 1.0
  */
 public class HeadingPanel extends JPanel {
+
+	/**
+	 * Maximum width of HeadingPanel
+	 */
+	private static final int MAX_WIDTH = 500;
+	
+	/**
+	 * Maximum height of HeadingPanel
+	 */
+	private static final int MAX_HEIGHT = 30;
+	
+	/**
+	 * Amount for first horizontal strut.
+	 */
+	private static final int FIRST_STRUT = 35;
+	
+	/**
+	 * Amount for second horizontal strut.
+	 */
+	private static final int SECOND_STRUT = 50;
+	
+	/**
+	 * Amount for third horizontal strut.
+	 */
+	private static final int THIRD_STRUT = 30;
+	
+	/**
+	 * Amount for fourth horizontal strut.
+	 */
+	private static final int FOURTH_STRUT = 35;
+	
+	/**
+	 * Amount for fifth horizontal strut.
+	 */
+	private static final int FIFTH_STRUT = 220;
+	
+	/**
+	 * Creates a HeadingPanel from four Strings.
+	 * 
+	 * @param first
+	 *            First String.
+	 * @param second
+	 *            Second String.
+	 * @param third
+	 *            Third String.
+	 * @param fourth
+	 *            Fourth String.
+	 */
 	public HeadingPanel(String first, String second, String third, String fourth) {
 		setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
 		setAlignmentY(Component.TOP_ALIGNMENT);
-		setMaximumSize(new Dimension(500, 30));
-		Component horizontalStrut1 = Box.createHorizontalStrut(35);
+		setMaximumSize(new Dimension(MAX_WIDTH, MAX_HEIGHT));
+		final Component horizontalStrut1 = Box.createHorizontalStrut(FIRST_STRUT);
 		add(horizontalStrut1);
 
-		JLabel lblItem = new JLabel(first);
+		final JLabel lblItem = new JLabel(first);
 		lblItem.setHorizontalAlignment(JLabel.CENTER);
 		add(lblItem);
 
-		Component horizontalStrut2 = Box.createHorizontalStrut(50);
+		final Component horizontalStrut2 = Box.createHorizontalStrut(SECOND_STRUT);
 		add(horizontalStrut2);
 
-		JLabel lblBuyingPrice = new JLabel(second);
+		final JLabel lblBuyingPrice = new JLabel(second);
 		lblBuyingPrice.setHorizontalAlignment(JLabel.CENTER);
 		add(lblBuyingPrice);
 
-		Component horizontalStrut3 = Box.createHorizontalStrut(30);
+		final Component horizontalStrut3 = Box.createHorizontalStrut(THIRD_STRUT);
 		add(horizontalStrut3);
 
-		JLabel lblAvailable = new JLabel(third);
+		final JLabel lblAvailable = new JLabel(third);
 		lblAvailable.setHorizontalAlignment(JLabel.CENTER);
 		add(lblAvailable);
 
-		Component horizontalStrut4 = Box.createHorizontalStrut(35);
+		final Component horizontalStrut4 = Box.createHorizontalStrut(FOURTH_STRUT);
 		add(horizontalStrut4);
 
-		JLabel lblToBuy = new JLabel(fourth);
+		final JLabel lblToBuy = new JLabel(fourth);
 		add(lblToBuy);
 
-		Component horizontalStrut5 = Box.createHorizontalStrut(220);
+		final Component horizontalStrut5 = Box.createHorizontalStrut(FIFTH_STRUT);
 		add(horizontalStrut5);
 	}
 }
--- SpaceFarmer/src/app/view/market/MarketScreen.java
+++ SpaceFarmer/src/app/view/market/MarketScreen.java
@@ -1,4 +1,12 @@
+/*This file holds the MarketScreen class, which represents the
+ * MarketPlace Screen in the game.
+ */
 package app.view.market;
+
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 import app.listener.ToggleBuyingAndSellingListener;
 import app.model.Game;
@@ -6,27 +14,39 @@ import app.model.Inventory;
 import app.model.MarketPlace;
 import app.view.CardName;
 import app.view.Screen;
-import app.view.market.BuyingPanel;
-import app.view.market.SellingPanel;
-
-import javax.swing.*;
 
 /**
  * This screen represents a marketplace with multiple buying/selling venues.
  * (WIP)
  * 
  * @author Ivory Assan, Mark McDonald
+ * @version 1.0
  */
 public class MarketScreen extends Screen {
 
+	/**
+	 * BuyingPanel associated with this MarketScreen.
+	 */
 	private BuyingPanel buyingPanel;
 
+	/**
+	 * SellingPanel associated with this MarketScreen.
+	 */
 	private SellingPanel sellingPanel;
 
+	/**
+	 * ToggleBuyingAndSellingListener associated with this Screen.
+	 */
 	private ToggleBuyingAndSellingListener toggleBuyingAndSellingListener;
 
+	/**
+	 * Error message label.
+	 */
 	private JLabel errorMessage;
 
+	/**
+	 * Constructs a MarketScreen with default values.
+	 */
 	public MarketScreen() {
 		name = CardName.MARKETPLACE_CARD;
 		errorMessage = new JLabel();
@@ -34,7 +54,7 @@ public class MarketScreen extends Screen {
 
 		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 
-		JLabel lblMarketPlace = new JLabel("Market Place");
+		final JLabel lblMarketPlace = new JLabel("Market Place");
 		add(lblMarketPlace);
 		add(errorMessage);
 
@@ -42,7 +62,7 @@ public class MarketScreen extends Screen {
 		sellingPanel = new SellingPanel(errorMessage);
 		sellingPanel.setVisible(false);
 
-		JButton toggleBuyAndSellButton = new JButton("Sell To Market");
+		final JButton toggleBuyAndSellButton = new JButton("Sell To Market");
 		toggleBuyingAndSellingListener = new ToggleBuyingAndSellingListener(
 				toggleBuyAndSellButton, buyingPanel, sellingPanel, null, null);
 		toggleBuyAndSellButton
@@ -50,7 +70,7 @@ public class MarketScreen extends Screen {
 
 		add(toggleBuyAndSellButton);
 
-		JPanel transactionPanel = new JPanel();
+		final JPanel transactionPanel = new JPanel();
 		transactionPanel.setLayout(new BoxLayout(transactionPanel,
 				BoxLayout.X_AXIS));
 		transactionPanel.add(buyingPanel);
@@ -59,18 +79,21 @@ public class MarketScreen extends Screen {
 	}
 
 	/**
-	 * Create the panel.
+	 * Constructs a MarketScreen using a MarketPlace.
+	 * @param marketPlace MarketPlace with which to construct the MarketScreen.
 	 */
 	public MarketScreen(MarketPlace marketPlace) {
 		this();
 		updateMarketPlace(marketPlace);
 	}
 
+	/**
+	 * Updates which MarketPlace this Screen displays.
+	 * @param marketPlace MarketPlace with which to display information.
+	 */
 	public void updateMarketPlace(MarketPlace marketPlace) {
-		buyingPanel.setSellingPanel(sellingPanel);
-		sellingPanel.setBuyingPanel(buyingPanel);
 
-		Inventory currentPlayersInventory = Game.getCurrentPlayer()
+		final Inventory currentPlayersInventory = Game.getCurrentPlayer()
 				.getInventory();
 
 		buyingPanel.setMarket(marketPlace);
